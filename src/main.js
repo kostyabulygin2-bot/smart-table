5d56e23c8985a8807d58ecd18be855c73e36291f
Konstantin Bulygin <kostyabulygin2@gmail.com>
2026-09-07 20:29:30 +0700

    Добавил сортировку

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 
 import {initPagination} from "./components/pagination.js"
+
+import {initSorting} from "./components/sorting.js"
 // @todo: подключение
 
 
@@ -40,6 +42,8 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
+    result = applySorting(result, state, action);
+
     result = applyPagination(result, state, action);
 
     sampleTable.render(result)
@@ -48,11 +52,16 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
+    before: ['header'],
     after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
 const applyPagination = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
